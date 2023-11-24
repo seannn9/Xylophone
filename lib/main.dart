157f7e25aece027a80e69c014cc99ce8aca51c9e
seannn9 <naeskirlu9@gmail.com>
2023-11-24 17:31:47 +0800

    added all colors and audio

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,89 @@ class MyApp extends StatelessWidget {
             body: SafeArea(
                 child: Column(
       children: [
-        TextButton(
-          onPressed: () {
-            final player = AudioPlayer();
-            player.play(AssetSource("note1.wav"));
-          },
-          child: Container(height: 50.0, color: Colors.red),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note1.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.orange,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note2.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.yellow,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note3.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note4.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.teal,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note5.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note6.wav"));
+            },
+            child: Container(),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.indigo,
+                shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero)),
+            onPressed: () {
+              final player = AudioPlayer();
+              player.play(AssetSource("note7.wav"));
+            },
+            child: Container(),
+          ),
         ),
       ],
     ))));
